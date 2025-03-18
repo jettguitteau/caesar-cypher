9de886b63ee725711edf94f68ac1f496f16cded6
Jett Guitteau <guitteau98@gmail.com>
2025-03-18 19:12:59 +0100

    Initial commit of basic program

---
+++ sneezing cat.py
@@ -0,0 +1,15 @@
+print('Asta is sneezing')
+print("Who is Asta?")
+
+cat = []
+
+cat.append("Bridgie")
+cat.append("Asta")
+
+print(str(cat[1]) + " is my roomate's cat.")
+print("Who is the cat wearing a tuxedo?")
+print("That is " + str(cat[0]) + ", she is always overdressed.")
+
+for cat in cat:
+    print(str(cat) + " is a good cat.")
+
